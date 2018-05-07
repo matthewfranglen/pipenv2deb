440f9f5482abf1ad39fe7d86a86d651fa7401c34
Matthew Franglen <matthew@franglen.org>
2018-05-07 23:52:09 +0100

    Really rough first draft

--- src/debian.py
+++ src/debian.py
@@ -1,11 +1,11 @@
 from tempfile import mkdtemp
+import os
+import pkg_resources
+from os import mkdir
 from os.path import join
 from shutil import copy, copytree, rmtree
 from jinja2 import Template
-from sh import (
-    dpkg_architecture,
-    git,
-)
+import sh
 
 
 def create_debian_package(settings):
@@ -18,6 +18,7 @@ class Directory:
 
     def __init__(self):
         self.path = mkdtemp()
+        mkdir(join(self.path, 'DEBIAN'))
 
     def __enter__(self):
         return self
@@ -27,13 +28,13 @@ class Directory:
 
     def prepare(self, settings):
         self.copy_project(settings)
-        self.install_pyenv(settings)
+        self.install_pyenv()
         self.install_python(settings)
         self.install_dependencies(settings)
         self.configure_dpkg(settings)
 
     def create(self):
-        pass
+        sh.dpkg_deb('--build', self.path, 'build.deb')
 
     def clean(self):
         if not self.path:
@@ -45,14 +46,19 @@ class Directory:
         copytree(settings.project, join(self.path, 'project'))
 
     def install_pyenv(self):
-        git('clone', 'https://github.com/pyenv/pyenv.git', join(self.path, 'pyenv')) # pylint: disable=too-many-function-args
+        sh.git.clone('https://github.com/pyenv/pyenv.git', join(self.path, 'pyenv'))
 
     def install_python(self, settings):
-        pass
-        # call pyenv install settings python version
+        pyenv = sh.Command(join(self.path, 'pyenv', 'bin', 'pyenv'))
+        pyenv.install(settings.python_version)
 
     def install_dependencies(self, settings):
-        pass
+        env = os.environ.copy()
+        env['PIPENV_VENV_IN_PROJECT'] = True
+        sh.bash('-c', 'cd {project} && pipenv install', _env=env)
 
     def configure_dpkg(self, settings):
-        pass
+        template = Template(pkg_resources.resource_string(__name__, 'templates/control.jinja'))
+        content = template.render(**settings)
+        with open(join(self.path, 'DEBIAN', 'control'), 'w') as handle:
+            handle.write(content)
