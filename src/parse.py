ce9603d1fa5564bad58995279c2f8cb3d69e88ee
Matthew Franglen <matthew@franglen.org>
2018-05-07 21:23:45 +0100

    Get parser reading scripts too
    It's really really simple.

--- src/parse.py
+++ src/parse.py
@@ -5,18 +5,21 @@ import ast
 
 def read_setup_details(data):
     parsed = ast.parse(data)
-    call = find_setup_call(parsed)
-    name = find_keyword(call, 'name')
-    version = find_keyword(call, 'version')
+    call = _find_setup_call(parsed)
+    name = _find_name(call)
+    version = _find_version(call)
+    scripts = _find_scripts(call)
 
-    return name, version
+    return name, version, scripts
 
 
-def find_setup_call(tree):
+def _find_setup_call(tree):
     finder = CallFinder()
     finder.visit(tree)
 
     for node in finder.calls:
+        if 'id' not in dir(node.func):
+            continue
         if node.func.id == 'setup':
             return node
     raise ValueError('Could not find call to setup in setup.py file')
@@ -30,8 +33,28 @@ class CallFinder(ast.NodeVisitor):
         self.calls.append(node)
 
 
-def find_keyword(call, arg):
+def _find_name(call):
+    return _find_keyword(call, 'name').s
+
+
+def _find_version(call):
+    try:
+        return _find_keyword(call, 'version').s
+    except:  # pylint: disable=bare-except
+        return None
+
+
+def _find_scripts(call):
+    try:
+        scripts = _find_keyword(call, 'scripts') or []
+
+        return [node.s for node in scripts.elts]
+    except:  # pylint: disable=bare-except
+        return []
+
+
+def _find_keyword(call, arg):
     for keyword in call.keywords:
         if keyword.arg == arg:
-            return keyword.value.s
+            return keyword.value
     raise ValueError('Could not find {arg} in call keywords'.format(arg=arg))
