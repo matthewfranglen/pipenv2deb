426fc8e7b6baf4e9ef0193ce2930b8b58c854134
Matthew Franglen <matthew@franglen.org>
2018-05-08 21:47:01 +0100

    Use pathlib instead of os.path

--- src/debian.py
+++ src/debian.py
@@ -1,6 +1,6 @@
 import os
 from os import mkdir
-from os.path import join
+from pathlib import Path
 from shutil import copytree, rmtree
 from tempfile import mkdtemp
 
@@ -17,15 +17,21 @@ def create_debian_package(settings):
 
 class PackageBuildDirectory:
     def __init__(self, settings):
-        self.path = self.pyenv_root = self.project_root = self.debian_root = None
+        self.path = self.pyenv_root = self.project_root = self.debian_root = self.env = None
         self.settings = settings
 
     def __enter__(self):
-        self.path = mkdtemp()
-        self.pyenv_root = join(self.path, 'pyenv')
-        self.project_root = join(self.path, 'project')
-        self.debian_root = join(self.path, 'DEBIAN')
+        self.path = Path(mkdtemp())
+        self.pyenv_root = self.path / 'pyenv'
+        self.project_root = self.path / 'project'
+        self.debian_root = self.path / 'DEBIAN'
         mkdir(self.debian_root)
+
+        env = os.environ.copy()
+        env['PYENV_ROOT'] = self.pyenv_root
+        env['PIPENV_VENV_IN_PROJECT'] = '1'
+        self.env = env
+
         return self
 
     def __exit__(self, exception_type, value, traceback):
@@ -60,18 +66,14 @@ class PackageBuildDirectory:
 
     def install_python(self):
         print('installing python...')
-        env = os.environ.copy()
-        env['PYENV_ROOT'] = self.pyenv_root
-        pyenv = sh.Command(join(self.path, 'pyenv', 'bin', 'pyenv'))
-        pyenv.install(self.settings.python_version, _env=env)
+        command_path = self.pyenv_root / 'bin' / 'pyenv'
+        pyenv = sh.Command(command_path)
+        pyenv.install(self.settings.python_version, _env=self.env)
 
     def install_dependencies(self):
         print('installing dependencies...')
-        env = os.environ.copy()
-        env['PYENV_ROOT'] = self.pyenv_root
-        env['PIPENV_VENV_IN_PROJECT'] = '1'
         os.chdir(self.project_root)
-        sh.pipenv('install', _env=env)
+        sh.pipenv('install', _env=self.env)
 
     def configure_dpkg(self):
         print('configuring debian metadata...')
@@ -79,5 +81,5 @@ class PackageBuildDirectory:
             pkg_resources.resource_string(__name__, 'templates/control.jinja')
         )
         content = template.render(**self.settings)
-        with open(join(self.path, 'DEBIAN', 'control'), 'w') as handle:
-            handle.write(content)
+        control_file = self.debian_root / 'control'
+        control_file.write_text(content)
